38924ec11e1f9ec58b497ad57aa962c9fdbf7009
@black.monster.maker <black.monster.maker@gmail.com>
2023-05-28 23:22:48 +0700

    sign up page

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  users: any[] = [];
 
+  userForm = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+
+  handleSubmitUser = (e: SubmitEvent) => {
+    e.preventDefault();
+
+    this.users.push(this.userForm.value);
+  };
+
+  get email() {
+    return this.userForm.get('email');
+  }
+
+  get password() {
+    return this.userForm.get('password');
+  }
 }
